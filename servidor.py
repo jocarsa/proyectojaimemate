2d6f5a8d36cc160a03eeee00b6a42f177be67b9a
= <info@josevicentecarratala.com>
2024-05-02 16:33:17 +0200

    2

--- servidor.py
+++ servidor.py
@@ -1,6 +1,6 @@
 #pip install Flask
 
-from flask import Flask, render_template
+from flask import Flask, render_template,request,url_for
 
 app = Flask(__name__)
 
@@ -19,6 +19,16 @@ def sobremi():
 @app.route('/contacto')
 def contacto():
     return render_template('contacto.html')
+
+@app.route('/procesa',methods=['POST'])
+def procesa():
+    if request.method == "POST":
+        
+        archivo = open("nombres.txt",'a')
+        archivo.write(request.form['nombre']+"\n")
+        archivo.close()
+        return "te proceso el formulario"
+        
 
 
 if __name__ == '__main__':
